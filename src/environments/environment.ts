4a8f28238201c10b07137c1dbd24c0ed21eecef0
Frederiko Cesar <35873365+frederikocmr@users.noreply.github.com>
2019-09-13 23:13:54 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyBm4H5YSBSPQjlNE6UYyRT3uVdqJwwt8sQ',
-    authDomain: 'linguicas-piracanjuba.firebaseapp.com',
-    databaseURL: 'https://linguicas-piracanjuba.firebaseio.com',
-    projectId: 'linguicas-piracanjuba',
-    storageBucket: 'linguicas-piracanjuba.appspot.com',
-    messagingSenderId: '29490854388'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
